0479fd4a8b2831e3ef69b9e605a66781909ccdc9
Jose Lages <jose.paivinhalages@gmail.com>
2023-03-03 23:16:22 +0000

    updated html structure

--- src/App.js
+++ src/App.js
@@ -10,6 +10,38 @@ const App = () => {
           <p>Shop Now</p>
         </div>
       </div>
+
+      <div className="category-container">
+        {/* <img/> */}
+        <div className="category-body-container">
+          <h2>Jackets</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        {/* <img/> */}
+        <div className="category-body-container">
+          <h2>Shoes</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        {/* <img/> */}
+        <div className="category-body-container">
+          <h2>Women</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        {/* <img/> */}
+        <div className="category-body-container">
+          <h2>Men</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
     </div>
   );
 };
